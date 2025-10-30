a149d511f2a03815f6d7ec87e9a739405d9385a8
Juan Rey Hernández <juanreyhc@gmail.com>
2025-10-30 13:46:28 -0600

    Implement filtering functionality in App component and enhance SearchFormSection for dynamic search capabilities

--- 02-react/src/App.jsx
+++ 02-react/src/App.jsx
@@ -1,42 +1,68 @@
-import { useState } from 'react'; 
-import { Header } from './components/Header.jsx';
-import { SearchFormSection } from './components/SearchFormSection.jsx';
-import { JobListings } from './components/JobListings.jsx';
-import { Pagination } from './components/Pagination.jsx';
-import { Footer } from './components/Footer.jsx';
+import { useState } from 'react'
 
-import jobsData from './data.json';
-//console.log(jobsData);
+import { Header } from './components/Header.jsx'
+import { Footer } from './components/Footer.jsx'
+import { Pagination } from './components/Pagination.jsx'
+import { SearchFormSection } from './components/SearchFormSection.jsx'
+import { JobListings } from './components/JobListings.jsx'
 
-const RESULT_PER_PAGE = 3;
+import jobsData from './data.json'
 
-function App() {  
-  const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = Math.ceil(jobsData.length / RESULT_PER_PAGE);
+const RESULTS_PER_PAGE = 4
 
-  const pagedResults = jobsData.slice(
-    (currentPage - 1) * RESULT_PER_PAGE,
-    currentPage * RESULT_PER_PAGE
-  );
+function App() {
+  const [filters, setFilters] = useState({
+      technology: '',
+      location: '',
+      experienceLevel: ''
+    })
+  const [textToFilter, setTextToFilter] = useState('')
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const jobsFilteredByFilters = jobsData.filter(job => {
+    return (
+      (filters.technology === '' || job.data.technology === filters.technology)
+    )
+  })
+
+  const jobsWithTextFilter = textToFilter === ''
+    ? jobsFilteredByFilters
+    : jobsFilteredByFilters.filter(job => {
+      return job.titulo.toLowerCase().includes(textToFilter.toLowerCase())
+    })
+
+  const totalPages = Math.ceil(jobsWithTextFilter.length / RESULTS_PER_PAGE)
+
+  const pagedResults = jobsWithTextFilter.slice(
+    (currentPage - 1) * RESULTS_PER_PAGE, // Página 1 -> 0, Página 2 -> 5, Página 3 -> 10
+    currentPage * RESULTS_PER_PAGE    // Página 1 -> 5, Página 2 -> 10, Página 3 -> 15
+  )
 
   const handlePageChange = (page) => {
-    console.log('Página cambiada a:', page);    
-    setCurrentPage(page);
-  };
+    setCurrentPage(page)
+  }
+
+  const handleSearch = (filters) => {
+    setFilters(filters)
+    setCurrentPage(1)
+  }
+
+  const handleTextFilter = (newTextToFilter) => {
+    setTextToFilter(newTextToFilter)
+    setCurrentPage(1)
+  }
 
   return (
     <>
       <Header />
 
       <main>
-        <SearchFormSection />
+        <SearchFormSection onSearch={handleSearch} onTextFilter={handleTextFilter} />
 
         <section>
           <JobListings jobs={pagedResults} />
-
           <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
         </section>
-
       </main>
 
       <Footer />
